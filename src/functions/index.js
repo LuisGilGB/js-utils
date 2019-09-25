fc68f197e758d4061db9c58dd6191355988cc93a
LuisGilGB <luis.gilgb@gmail.com>
2019-09-25 13:01:55 +0200

    Added isFunction function

--- src/functions/index.js
+++ src/functions/index.js
@@ -3,8 +3,9 @@ const defaultMapper = require('./defaultMapper');
 const exists = require('./exists');
 const objectify = require('./objectify');
 const isNull = require('./isNull');
-const isArray = require('./isArray');
 const isString = require('./isString');
+const isArray = require('./isArray');
+const isFunction = require('./isFunction');
 const isTruthy = require('./isTruthy');
 const isNullOrUndefined = require('./isNullOrUndefined');
 const isEmptyArray = require('./isEmptyArray');
@@ -15,8 +16,9 @@ module.exports = {
     exists,
     objectify,
     isNull,
-    isArray,
     isString,
+    isArray,
+    isFunction,
     isTruthy,
     isNullOrUndefined,
     isEmptyArray,
---
+++ src/functions/isFunction.js
@@ -0,0 +1,6 @@
+/*
+ * Returns true if the input is a function.
+ */
+const isFunction = input => typeof input === 'function';
+
+module.exports = isFunction;
