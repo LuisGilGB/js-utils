216eca82530a828d8739747a69d55e49619ec56f
LuisGilGB <luis.gilgb@gmail.com>
2019-09-25 13:09:29 +0200

    Improved isString function logic

--- src/functions/isString.js
+++ src/functions/isString.js
@@ -1,6 +1,6 @@
 /*
  * Returns true if the input is a string.
  */
-const isString = input => typeof input === 'string';
+const isString = input => typeof input === 'string' || input instanceOf String;
 
 module.exports = isString;
