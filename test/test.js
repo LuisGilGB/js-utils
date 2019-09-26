022dd5300c914d4786603c7043f2698fd8bd1c37
LuisGilGB <luis.gilgb@gmail.com>
2019-09-26 18:12:32 +0200

    Automatically loading valid test files inside test-functions directory

--- test/test.js
+++ test/test.js
@@ -1,8 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+const testFunctionsDir = path.join(__dirname, 'test-functions');
+
 const testFunction = (name) => {
     describe(`Function: ${name}`, () => {
         require(`./test-functions/test-${name}`);
     });
 }
 
-testFunction('defaultMapper');
-testFunction('exists');
+describe('----- js-utils tests -----', () => {
+    let testFiles = [];
+
+    before((done) => {
+        console.log('');
+        console.log('Loading test files...');
+        fs.readdir(testFunctionsDir, (err, files) => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log('Test files successfully loaded');
+            testFiles = files.filter(fileName => fileName.startsWith('test-') && fileName.endsWith('.js'));
+            console.log('');
+            done();
+        });
+    });
+
+    it('Testing js-utils functions...', () => {
+        testFiles.forEach(fileName => {
+            testFunction(fileName.slice('5', '-3'));
+        });
+    });
+});
