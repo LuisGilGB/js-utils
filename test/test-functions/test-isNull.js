6ef7c59b23df06fa52bbd30d99a7e712ba85429d
LuisGilGB <luis.gilgb@gmail.com>
2019-09-30 12:38:14 +0200

    Fix for isNull tests

--- test/test-functions/test-isNull.js
+++ test/test-functions/test-isNull.js
@@ -17,52 +17,56 @@ const arrayNumbersExpects = false;
 const arrayStringsExpects = false;
 const arrayObjectsExpects = false;
 const objectExpects = false;
+const functionExpects = false;
 
-it(`Undefined returns `, () => {
+it(`Undefined returns ${undefinedExpects}`, () => {
     assert.equal(isNull(undefined), undefinedExpects);
 });
-it(`Null returns `, () => {
+it(`Null returns ${nullExpects}`, () => {
     assert.equal(isNull(null), nullExpects);
 });
-it(`False returns `, () => {
+it(`False returns ${falseExpects}`, () => {
     assert.equal(isNull(false), falseExpects);
 });
-it(`True returns `, () => {
+it(`True returns ${trueExpects}`, () => {
     assert.equal(isNull(true), trueExpects);
 });
-it(`Empty string returns `, () => {
+it(`Empty string returns ${emptyStringExpects}`, () => {
     assert.equal(isNull(''), emptyStringExpects);
 });
-it(`0 returns `, () => {
+it(`0 returns ${zeroExpects}`, () => {
     assert.equal(isNull(0), zeroExpects);
 });
-it(`NaN returns `, () => {
+it(`NaN returns ${nanExpects}`, () => {
     assert.equal(isNull(NaN), nanExpects);
 });
-it(`Infinity returns `, () => {
+it(`Infinity returns ${infinityExpects}`, () => {
     assert.equal(isNull(Infinity), infinityExpects);
 });
-it(`Empty array returns `, () => {
+it(`Empty array returns ${emptyArrayExpects}`, () => {
     assert.equal(isNull([]), emptyArrayExpects);
 });
-it(`Empty object returns `, () => {
+it(`Empty object returns ${emptyObjectExpects}`, () => {
     assert.equal(isNull({}), emptyObjectExpects);
 });
-it(`Arbitrary string returns `, () => {
+it(`Arbitrary string returns ${stringExpects}`, () => {
     assert.equal(isNull('Arbitrary'), stringExpects);
 });
-it(`Arbitrary number returns `, () => {
+it(`Arbitrary number returns ${numberExpects}`, () => {
     assert.equal(isNull(1234), numberExpects);
 });
-it(`Array of numbers returns `, () => {
+it(`Array of numbers returns ${arrayNumbersExpects}`, () => {
     assert.equal(isNull([1,2,3]), arrayNumbersExpects);
 });
-it(`Array of strings returns `, () => {
+it(`Array of strings returns ${arrayStringsExpects}`, () => {
     assert.equal(isNull(['a','b','c']), arrayStringsExpects);
 });
-it(`Array of objects returns `, () => {
+it(`Array of objects returns ${arrayObjectsExpects}`, () => {
     assert.equal(isNull([{v:1},{v:2},{v:3}]), arrayObjectsExpects);
 });
-it(`Arbitrary object returns `, () => {
+it(`Arbitrary object returns ${objectExpects}`, () => {
     assert.equal(isNull({a:1,b:2,c:3}), objectExpects);
+});
+it(`Arbitrary function returns ${functionExpects}`, () => {
+    assert.equal(isNull(() => 'I am a function'), functionExpects);
 });
