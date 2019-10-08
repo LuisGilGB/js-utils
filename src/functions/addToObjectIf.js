a0c69d0d207193f7dd97807e14f783ab7413d0bc
LuisGilGB <luis.gilgb@gmail.com>
2019-10-08 17:14:06 +0200

    Improved addToObjectIf logic with non empty string key check

--- src/functions/addToObjectIf.js
+++ src/functions/addToObjectIf.js
@@ -1,7 +1,9 @@
 /*
  * Add to the input object a value at the field specified by a key if successfully resolves
- * the given condition.
+ * the given condition. The key must be a valid non empty string.
  */
-const addToObjectIf = (condFn, obj = {}, value, key) => condFn(value) ? {...obj, [key]: value} : obj;
+const {isNonEmptyString} = require('../../index');
+
+const addToObjectIf = (condFn, obj = {}, value, key) => isNonEmptyString(key) && condFn(value) ? {...obj, [key]: value} : obj;
 
 module.exports = addToObjectIf;
