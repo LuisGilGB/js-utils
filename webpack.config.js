365557546a82fa72d32a535fcf6a6c6631529371
LuisGilGB <luis.gilgb@gmail.com>
2019-10-21 17:26:29 +0200

    Target library to UMD

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,10 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
+        library: 'js-utils',
+        libraryTarget: 'umd',
         filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
+        path: path.resolve(__dirname, 'dist'),
+        globalObject: 'this'
     }
 }
